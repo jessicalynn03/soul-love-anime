3ec29edb9b71b653941fc4059656fa2f42899d28
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-04-28 09:16:32 -0400

    Created function when nav link is clicked component renders on screen

--- soul-love-anime/src/App.js
+++ soul-love-anime/src/App.js
@@ -1,47 +1,47 @@
-// import './App.css';
-// import React, { Component } from 'react';
-// import Anime from "./components/Anime";
-// import Post from "./components/Post";
+import './App.css';
+import React, { Component } from 'react';
+import Anime from "./components/Anime";
+import Post from "./components/Post";
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       userChoice: "",
-//     }
-//   }
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userChoice: "",
+    }
+  }
   
-//   picChoice = (choice) => {
-//     // console.info("I was clicked");
-//     if (choice === "Home Page") {
-//       this.setState({userChoice: <Anime />})
-//     }
-//     else if (choice === "Blog Page"){
-//       this.setState({userChoice: <Post />})
-//     }
-//   }
-//   render() {
+  picChoice = (choice) => {
+    // console.info("I was clicked");
+    if (choice === "Home Page") {
+      this.setState({userChoice: <Anime />})
+    }
+    else if (choice === "Blog Page"){
+      this.setState({userChoice: <Post />})
+    }
+  }
+  render() {
 
-//     return (
-//       <div className="App">
-//         <h1>SOUL-LOVE ANIME</h1>
-//         <div>
+    return (
+      <div className="App">
+        <h1>SOUL-LOVE ANIME</h1>
+        <div>
 
-//           <div onClick={ () => {this.picChoice("Home Page")}}>Home Page</div>
-//       <hr>
-//       </hr>
-//           <div  onClick={() => {this.picChoice("Blog Page")}}>Blog Page</div>
+          <div onClick={ () => {this.picChoice("Home Page")}}>Home Page</div>
+      <hr>
+      </hr>
+          <div  onClick={() => {this.picChoice("Blog Page")}}>Blog Page</div>
 
-//         </div>
-//         <form>
+        </div>
+        <form>
 
-//         </form>
+        </form>
         
-//           {this.state.userChoice}
+          {this.state.userChoice}
       
-//       </div>
-//     );
-//   }
-// }
+      </div>
+    );
+  }
+}
 
-// export default App;
+export default App;
